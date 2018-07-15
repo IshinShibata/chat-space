f3002f8ac021efdb07c49602ec789b543dd32e61
IshinShibata <shibataishin@shibata-Mac.local>
2018-07-15 21:22:42 +0900

    mesaagejs_contoroller_routes_indent_edit

--- app/assets/javascripts/channels/message.js
+++ app/assets/javascripts/channels/message.js
@@ -1,8 +1,8 @@
 $(function(){
   function buildHTML(message){
-    var buildImage = '';
+      var buildImage = '';
   if (message.image) {
-    buildImage = `<img src="${message.image}">`;
+      buildImage = `<img src="${message.image}">`;
   }
   var html =`
 <div class="message">
@@ -18,10 +18,10 @@ $(function(){
     return html;
   }
  $('#item_form').on('submit', function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action');
-    $.ajax({
+  e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr('action');
+  $.ajax({
       url: url,
       type: "POST",
       data: formData,
@@ -29,14 +29,14 @@ $(function(){
       processData: false,
       contentType: false
   })
-    .done(function(data){
+  .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html);
       $('.formarea__forms__form').val('');
-     $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 1000, 'swing');
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 1000, 'swing');
       $('.formarea__forms__submit-button').prop("disabled", false);
-    })
-    .fail(function(){
+  })
+  .fail(function(){
       alert('error');
 })
 })
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,11 +1,14 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: [:edit, :update]
+
   def index
   end
+
   def new
       @group = Group.new
       @group.users << current_user
   end
+
   def create
       @group = Group.new(group_params)
       if @group.save
@@ -14,6 +17,7 @@ class GroupsController < ApplicationController
         render :new
       end
   end
+
   def update
     if @group.update(group_params)
       redirect_to group_messages_path(@group), notice: 'グループを編集しました'
@@ -26,6 +30,7 @@ class GroupsController < ApplicationController
   def group_params
     params.require(:group).permit(:name, { :user_ids => [] })
   end
+
   def set_group
     @group = Group.find(params[:id])
   end
