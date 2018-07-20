0f84608d4972c2cb09a4cd8cf84172a69ba3ae77
IshinShibata <shibataishin@shibata-Mac.local>
2018-07-20 12:08:12 +0900

    インデント一部修正

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -2,33 +2,33 @@ $(function() {
 
 var search_list = $("#user-search-result");
 function appendUser(user) {
-   var html =
-`
+    var html =
+  `
   <div id="user-search-result">
   <div class="chat-group-user clearfix">
     <p class="chat-group-user__name">${user.name}</p>
     <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${ user.name }>追加</a>
   </div>
   </div>
-`
+  `
 search_list.append(html);
 }
 
 function appendNoUser(user){
     var html =
-`<div class="chat-group-user clearfix">${user}</div>`
-$("#user-search-result").append(html)
+    `<div class="chat-group-user clearfix">${user}</div>`
+    $("#user-search-result").append(html)
 }
 
 function addUser(name,id) {
     var html =
-`
+  `
   <div class ='chat-group-user clearfix js-chat-member' id= 'chat-group-user-${id}'>
     <input name = 'group[user_ids][]', type = 'hidden', value = '${id}'>
     <p class='chat-group-user__name'>${name}</p>
     <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn' data-user-id=${id} data-user-name=${name}> 削除</a>
   </div>
-`
+  `
   $("#chat-group-users").append(html)
 }
 
@@ -41,10 +41,10 @@ $("#user-search-field").on("keyup", function() {
       dataType: 'json'
     })
    .done(function(users) {
-     $("#user-search-result").empty();
-     if (users.length !== 0) {
+       $("#user-search-result").empty();
+       if (users.length !== 0) {
        users.forEach(function(user){
-         appendUser(user);
+        appendUser(user);
        });
      }
      else {
